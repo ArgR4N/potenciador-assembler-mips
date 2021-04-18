5db885397877f481a0928517ae3dfa25a7e796fb
ramiro <ramiroreinaldo12@gmail.com>
2021-04-18 00:28:02 -0300

    arreglos a potenciador.asm

--- potenciador.asm
+++ potenciador.asm
@@ -4,6 +4,7 @@
     promptquantity: .asciiz "ingrese la cantidad de potencias que quiere hacer con el numero (con un maximo de 29 veces):\n"
     textoarreglo: .asciiz "se llego al limite del programa\n"
     numprueba: .word 0
+    resultado: .asciiz "las potencias son:\n"
 .text
 .globl main
 main:
@@ -14,7 +15,6 @@ main:
     syscall #tomar el valor
     move $t0, $v0
     move $t2, $v0#crea una copia del valor dado por le usuario y lo pone en $t2
-    add $t3, $t0, $t2 #crea un valor diferente al tomado y lo guarda a $t3
     li $v0, 4
     la $a0, promptquantity
     syscall # mover el valor del numero a $t0 y imprimir la pregunta de la cantidad de potencias
@@ -22,30 +22,30 @@ main:
     syscall
     move $t1, $v0
     li $v0, 4
-    la $a0, linea #imprimir linea vacia
+    la $a0, resultado
     syscall
 inicio:
     li $v0, 1
     move $a0, $t0
-    move $t5, $t0
+    move $t4, $t0
     syscall #imprimir el numero
     mul $t0, $t0, $t2
     li $v0, 4
     la $a0, linea
     syscall
     addi $t1, $t1, -1
-    syscall
+    j loop
 loop:
     beq $t1, $zero, exit
-    div $t4, $t0, $t5
- bne $t4, $t2, arreglo
+    div $t3, $t0, $t4
+    bne $t3, $t2, arreglo
     li $v0, 1
     move $a0, $t0
+    move $t4, $t0
     syscall #iniciar el bucle y imprimir el numero
     mul $t0, $t0, $t2
     li $v0, 4
     la $a0, linea
-    move $t5, $a0
     syscall
     addi $t1, $t1, -1
     j loop
